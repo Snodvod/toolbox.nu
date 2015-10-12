b93463f3c3497b93061d571bb4d9b59a43c64860
kristofsw <kristof.sweerts@gmail.com>
2015-10-12 15:00:00 +0200

    Homepage Changes #2
    Added search form to search for tools. Also added a header for future
    plans.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,13 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
         <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic,600italic,300italic,700italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/app.css">
     </head>
     <body>
         @yield('content')
     </body>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/js/backstretch.js" type=text/javascript></script>
+    <script src="/js/main.js" type=text/javascript></script>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,40 @@
             <div class="col col-50">
                 <div id="nav_auth">
                     @if (Auth::check())
-                        <a href="/auth/getLogout">Logout</a>
-                    @else
-                        <a href="/auth/register">Registreren</a>
-                        <a href="/auth/login">Login</a>
-                    @endif
+                    <a href="/auth/getLogout">Logout</a> @else
+                    <a href="/auth/register">Registreren</a>
+                    <a href="/auth/login">Login</a> @endif
                 </div>
             </div>
         </div>
     </div>
+    <div id="landing_content">
+        <div id="landing_title">
+            <h1>Add generic title here</h1>
+            <p id="subtext">Hic vidisse ut ullamco, ipsum hic excepteur ut irure. Culpa non ab aliqua 
+                doctrina, veniam se eu noster nescius. Ea si familiaritatem, lorem ab fabulas ad 
+                vidisse dolore incididunt iis iis eram voluptatibus. Commodo dolore nulla 
+                offendit illum est enim nescius comprehenderit.</p>
+        </div>
+        <div id="landing_search">
+            <form>
+                <div id="search_box">
+                    <p>Ik zoek een</p>
+                    <input placeholder="tool..">
+                    <p>van</p>
+                    <input placeholder="Begindatum">
+                    <p>tot</p>
+                    <input placeholder="Einddatum">
+                    <div class="search_button">
+                        <button type="submit">Zoeken</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="main_content">
+
 </div>
 
 @stop
