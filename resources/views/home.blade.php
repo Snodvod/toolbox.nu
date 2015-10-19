ebbf59a6e8fba0953ce3768ea7a15a43c52d996d
kristofsw <kristof.sweerts@gmail.com>
2015-10-19 11:15:09 +0200

    More navigation changes
    Added borders, and highlight arrows

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,48 +1,50 @@
 @extends('app') @section('content')
 
 <div id="landing">
-    <div class="nav">
-        <div class="col col-65 centered">
-            <div class="col col-50">
-                <div id="nav_logo">
-                    <a href="/"><img src="/img/logo.svg"></a>
-                </div>
-                <div id="nav_main">
-                    <a href="/tools">Tools</a>
-                </div>
-            </div>
-            <div class="col col-50">
-                <div id="nav_auth">
-                    @if (Auth::check())
-                    <a href="/auth/getLogout">Logout</a> @else
-                    <a href="/auth/register">Registreren</a>
-                    <div id="showlogin">Login</div> @endif
-                    <div id="login_box">
-                        <form method="POST" action="/auth/login">
-                            {!! csrf_field() !!}
-                            <div>
-                                Email
-                                <input type="email" name="email" value="{{ old('email') }}">
-                            </div>
-
-                            <div>
-                                Password
-                                <input type="password" name="password" id="password">
-                            </div>
-
-                            <div class="checkbox">
-                                <input type="checkbox" name="remember"> Remember Me
-                            </div>
-
-                            <div>
-                                <button type="submit">Login</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    @section('navigation')
+    <div id="nav_logo">
+        <a href="/"><img src="/img/logo.svg"></a>
+    </div>
+    <div id="nav_main">
+        <div class="nav_item">
+            <a href="/tools">Tools</a>
         </div>
     </div>
+    <div id="nav_auth">
+        @if (Auth::check())
+        <div class="nav_item">
+            <a href="/auth/getLogout">Logout</a>
+        </div>@else
+        <div class="nav_item">
+            <a href="/auth/register">Registreren</a>
+        </div>
+        <div class="nav_item">
+            <div id="showlogin">Login</div>
+        </div>@endif
+        <div id="login_box">
+            <form method="POST" action="/auth/login">
+                {!! csrf_field() !!}
+                <div>
+                    Email
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+
+                <div>
+                    Password
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div class="checkbox">
+                    <input type="checkbox" name="remember"> Remember Me
+                </div>
+
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    @stop
     <div id="landing_content">
         <!--<div id="landing_title">
             <h1>Add generic title here</h1>
