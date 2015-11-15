c07be11ec48237b2c3e64b5a5c09f068a0e6a077
FrozenFalcon <senne.devalck@gmail.com>
2015-11-15 15:21:57 +0100

    search toegepast

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+        var searchval = 0;
+        var startval = 0;
+        var stopval = 0;
+    $("#homesearchgroup div button").click(function (e) {
+        e.preventDefault();
+        searchval = $('#homesearchgroup div #homesearch').val();
+        startval = $('#homesearchgroup div #homestart').val();
+        stopval = $('#homesearchgroup div #homestop').val();
+        console.log(searchval);
+        console.log(startval);
+        console.log(stopval);
+        document.location.href='/tools?search=' + searchval + '&start=' + startval + '&stop=' + stopval;
+    });
     $(".action.notstatus").click(function (e) {
         e.preventDefault();
         //$(this).addClass("redeemed").text("Redeemed");
