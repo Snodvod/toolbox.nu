0350489edb0a5e18cca0caeaf47c2387fd58f782
Ino Van Winckel <inovanwinckel@hotmail.com>
2015-11-15 22:01:09 +0100

    Seeding for real men

--- database/seeds/JobTableSeeder.php
+++ database/seeds/JobTableSeeder.php
@@ -11,12 +11,28 @@ class JobTableSeeder extends Seeder
      */
     public function run()
     {
-    	for ($i=1; $i<=20 ; $i++) {
-    		DB::table('jobs')->insert([
-	        	'name' => str_random(10),
-	        	'difficulty' => rand(1,5),
-	        	'description' => str_random(100),
-        	]);
-    	}
+    	DB::table('jobs')->insert([
+            'name' => 'timmeren',
+            'difficulty' => 3,
+            'description' => 'Iets met een hamer, hout en nagels'
+        ]);
+
+        DB::table('jobs')->insert([
+            'name' => 'dakgoot repareren',
+            'difficulty' => 3,
+            'description' => 'Algemene reparatie van een dakgoot'
+        ]);
+
+        DB::table('jobs')->insert([
+                'name' => 'muurtje metsen',
+                'difficulty' => 4,
+                'description' => 'Met mortel een mooie muur maken, zoals voor een huis.'
+        ]);
+
+        DB::table('jobs')->insert([
+                'name' => 'Boomstronken zagen en splitten',
+                'difficulty' => 3,
+                'description' => 'Hout van omgezaagde bomen in stukken zagen en splitten zodat het als haardhout kan worden gebruikt.'
+        ]);
     }
 }
--- database/seeds/ToolTableSeeder.php
+++ database/seeds/ToolTableSeeder.php
@@ -11,55 +11,49 @@ class ToolTableSeeder extends Seeder
      */
     public function run()
     {
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
 	        'user_id' => 1,
 	        'name' => 'Handzaag',
 	        'brand' => 'Stanley',
-	        'bought_at' => date('Y-m-d H:i:s', $int),
+	        'bought_at' => date('Y-m-d H:i:s'),
             'category_id' => 3,
 	    ]);
 
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
             'user_id' => 1,
             'name' => 'Metaalzaag',
             'brand' => 'Stanley',
-            'bought_at' => date('Y-m-d H:i:s', $int),
+            'bought_at' => date('Y-m-d H:i:s'),
             'category_id' => 3,
         ]);
 
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
-            'user_id' => 1,
+            'user_id' => 2,
             'price' => 14,
             'name' => 'Multibit met ratel',
             'brand' => 'Stanley',
-            'bought_at' => date('Y-m-d H:i:s', $int),
+            'bought_at' => date('Y-m-d H:i:s'),
             'category_id' => 7,
         ]);
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
-            'user_id' => 1,
+            'user_id' => 2,
             'name' => 'Schroevendraaierset Cushiongrip 6-delig',
             'brand' => 'Stanley',
-            'bought_at' => date('Y-m-d H:i:s', $int),
+            'bought_at' => date('Y-m-d H:i:s'),
             'category_id' => 7,
         ]);
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
             'user_id' => 1,
             'name' => 'Klauwhamer Steelmaster 450gr',
             'brand' => 'Stanley',
-            'bought_at' => date('Y-m-d H:i:s', $int),
+            'bought_at' => date('Y-m-d H:i:s'),
             'category_id' => 1,
         ]);
-        $int = mt_rand(1262055681,1262055681);
         DB::table('tools')->insert([
-            'user_id' => 1,
+            'user_id' => 2,
             'name' => 'Voorhamer 4kg',
             'brand' => 'Polet',
-            'bought_at' => date('Y-m-d H:i:s', $int),
+            'bought_at' => date('Y-m-d H:i:s'),
             'about' => 'Groten hamer jonge!',
             'category_id' => 1,
         ]);
